72c6f0513c6f5a04f70994590174bf0479e3dc14
Robert911hit <anookohrobert8@gmail.com>
2025-08-03 16:09:20 -0700

    Add Create React App (CRA) frontend for Lovable Open

---
+++ frontend-cra/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Lovable Open (CRA)</h1>
+      <p>Welcome to the CRA version of Lovable Open!</p>
+    </div>
+  );
+}
+
+export default App;
